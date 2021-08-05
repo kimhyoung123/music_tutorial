2fbdaae844df3b400b8cbfac2d9f0e0b03a43d8f
kimhyoung123 <84779339+kimhyoung123@users.noreply.github.com>
2021-08-06 02:24:40 +0900

    Add files via upload

--- lib/page_manager.dart
+++ lib/page_manager.dart
@@ -1,40 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
-class PageManager {
-  final progressNotifier = ValueNotifier<ProgressBarState>(
-    ProgressBarState(
-      current: Duration.zero,
-      buffered: Duration.zero,
-      total: Duration.zero,
-    ),
-  );
+import 'notifiers/play_button_notifier.dart';
+import 'notifiers/progress_notifier.dart';
+import 'notifiers/repeat_button_notifier.dart';
 
-  final buttonNotifier = ValueNotifier<ButtonState>(ButtonState.paused);
-  static const url = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3';
+class PageManager {
+  final currentSongTitleNotifier = ValueNotifier<String>('');
+  final playlistNotifier = ValueNotifier<List<String>>([]);
+  final progressNotifier = ProgressNotifier();
+  final repeatButtonNotifier = RepeatButtonNotifier();
+  final isFirstSongNotifier = ValueNotifier<bool>(true);
+  final playButtonNotifier = PlayButtonNotifier();
+  final isLastSongNotifier = ValueNotifier<bool>(true);
+  final isShuffleModeEnabledNotifier = ValueNotifier<bool>(false);
+
   late AudioPlayer _audioPlayer;
+  late ConcatenatingAudioSource _playlist;
+
   PageManager() {
     _init();
   }
+
   void _init() async {
     _audioPlayer = AudioPlayer();
-    await _audioPlayer.setUrl(url);
+    _setInitialPlaylist();
+    _listenForChangesInPlayerState();
+    _listenForChangesInPlayerPosition();
+    _listenForChangesInBufferedPosition();
+    _listenForChangesInTotalDuration();
+    _listenForChangesInSequenceState();
+  }
+
+  void _setInitialPlaylist() async {
+    const prefix = 'https://www.soundhelix.com/examples/mp3';
+    final song1 = Uri.parse('$prefix/SoundHelix-Song-1.mp3');
+    final song2 = Uri.parse('$prefix/SoundHelix-Song-2.mp3');
+    final song3 = Uri.parse('$prefix/SoundHelix-Song-3.mp3');
+    _playlist = ConcatenatingAudioSource(children: [
+      AudioSource.uri(song1, tag: 'Song 1'),
+      AudioSource.uri(song2, tag: 'Song 2'),
+      AudioSource.uri(song3, tag: 'Song 3'),
+    ]);
+    await _audioPlayer.setAudioSource(_playlist);
+  }
+
+  void _listenForChangesInPlayerState() {
     _audioPlayer.playerStateStream.listen((playerState) {
       final isPlaying = playerState.playing;
       final processingState = playerState.processingState;
       if (processingState == ProcessingState.loading ||
           processingState == ProcessingState.buffering) {
-        buttonNotifier.value = ButtonState.loading;
+        playButtonNotifier.value = ButtonState.loading;
       } else if (!isPlaying) {
-        buttonNotifier.value = ButtonState.paused;
+        playButtonNotifier.value = ButtonState.paused;
       } else if (processingState != ProcessingState.completed) {
-        buttonNotifier.value = ButtonState.playing;
-      } else { // completed
+        playButtonNotifier.value = ButtonState.playing;
+      } else {
         _audioPlayer.seek(Duration.zero);
         _audioPlayer.pause();
       }
     });
+  }
 
+  void _listenForChangesInPlayerPosition() {
     _audioPlayer.positionStream.listen((position) {
       final oldState = progressNotifier.value;
       progressNotifier.value = ProgressBarState(
@@ -43,7 +72,9 @@ class PageManager {
         total: oldState.total,
       );
     });
+  }
 
+  void _listenForChangesInBufferedPosition() {
     _audioPlayer.bufferedPositionStream.listen((bufferedPosition) {
       final oldState = progressNotifier.value;
       progressNotifier.value = ProgressBarState(
@@ -52,7 +83,9 @@ class PageManager {
         total: oldState.total,
       );
     });
+  }
 
+  void _listenForChangesInTotalDuration() {
     _audioPlayer.durationStream.listen((totalDuration) {
       final oldState = progressNotifier.value;
       progressNotifier.value = ProgressBarState(
@@ -62,9 +95,39 @@ class PageManager {
       );
     });
   }
-  void play() {
+
+  void _listenForChangesInSequenceState() {
+    _audioPlayer.sequenceStateStream.listen((sequenceState) {
+      if (sequenceState == null) return;
+
+      // update current song title
+      final currentItem = sequenceState.currentSource;
+      final title = currentItem?.tag as String?;
+      currentSongTitleNotifier.value = title ?? '';
+
+      // update playlist
+      final playlist = sequenceState.effectiveSequence;
+      final titles = playlist.map((item) => item.tag as String).toList();
+      playlistNotifier.value = titles;
+
+      // update shuffle mode
+      isShuffleModeEnabledNotifier.value = sequenceState.shuffleModeEnabled;
+
+      // update previous and next buttons
+      if (playlist.isEmpty || currentItem == null) {
+        isFirstSongNotifier.value = true;
+        isLastSongNotifier.value = true;
+      } else {
+        isFirstSongNotifier.value = playlist.first == currentItem;
+        isLastSongNotifier.value = playlist.last == currentItem;
+      }
+    });
+  }
+
+  void play() async {
     _audioPlayer.play();
   }
+
   void pause() {
     _audioPlayer.pause();
   }
@@ -76,19 +139,47 @@ class PageManager {
   void dispose() {
     _audioPlayer.dispose();
   }
-}
 
-class ProgressBarState {
-  ProgressBarState({
-    required this.current,
-    required this.buffered,
-    required this.total,
-  });
-  final Duration current;
-  final Duration buffered;
-  final Duration total;
-}
+  void onRepeatButtonPressed() {
+    repeatButtonNotifier.nextState();
+    switch (repeatButtonNotifier.value) {
+      case RepeatState.off:
+        _audioPlayer.setLoopMode(LoopMode.off);
+        break;
+      case RepeatState.repeatSong:
+        _audioPlayer.setLoopMode(LoopMode.one);
+        break;
+      case RepeatState.repeatPlaylist:
+        _audioPlayer.setLoopMode(LoopMode.all);
+    }
+  }
 
-enum ButtonState {
-  paused, playing, loading
+  void onPreviousSongButtonPressed() {
+    _audioPlayer.seekToPrevious();
+  }
+
+  void onNextSongButtonPressed() {
+    _audioPlayer.seekToNext();
+  }
+
+  void onShuffleButtonPressed() async {
+    final enable = !_audioPlayer.shuffleModeEnabled;
+    if (enable) {
+      await _audioPlayer.shuffle();
+    }
+    await _audioPlayer.setShuffleModeEnabled(enable);
+  }
+
+  void addSong() {
+    final songNumber = _playlist.length + 1;
+    const prefix = 'https://www.soundhelix.com/examples/mp3';
+    final song = Uri.parse('$prefix/SoundHelix-Song-$songNumber.mp3');
+    _playlist.add(AudioSource.uri(song, tag: 'Song $songNumber'));
+  }
+
+  void removeSong() {
+    final index = _playlist.length - 1;
+    if (index < 0) return;
+    _playlist.removeAt(index);
+  }
 }
